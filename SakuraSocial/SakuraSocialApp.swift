db36c52b5a46acc5fc33c674f76d841e798ccd93
seiba-atsushi <riki21061216@gmail.com>
2023-04-25 23:16:01 +0900

    Initial Commit

---
+++ SakuraSocial/SakuraSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakuraSocialApp.swift
+//  SakuraSocial
+//
+//  Created by 本村力希 on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SakuraSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
